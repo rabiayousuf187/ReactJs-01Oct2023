4f47928abbe19446a79e2bdb196ed252168c58a1
Rabia Yousuf <53699291+rabiayousuf187@users.noreply.github.com>
2023-10-01 10:28:43 +0500

    Materila UI install ans Bottom Nav. used inside APPjs

--- src/App.js
+++ src/App.js
@@ -21,16 +21,24 @@ import {Collapse,
   DropdownItem,
   NavbarText,Card, CardBody, CardTitle,CardSubtitle,CardText, Button } from 'reactstrap'
 
+  // import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 function App() {
 
+  const [value, setValue] = React.useState(0);
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
   return (
     <div className="App">
-      <div>
+
       <Navbar>
         <NavbarBrand href="/">reactstrap</NavbarBrand>
         <NavbarToggler onClick={toggle} />
@@ -59,7 +67,7 @@ function App() {
           <NavbarText>Simple Text</NavbarText>
         </Collapse>
       </Navbar>
-      </div>
+    
 
 
       <Heading title="Expense Tracer App" />
@@ -97,6 +105,20 @@ function App() {
           </Button>
         </CardBody>
       </Card>
+
+      <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </Box>
 
     </div>
   );
